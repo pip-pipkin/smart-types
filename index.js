1db460010f7e721498daa58155daf33309b19228
Pip <pip.pipkin@gmail.com>
2016-06-24 23:27:45 -0500

    updated tests

--- index.js
+++ index.js
@@ -1,56 +1,131 @@
 'use strict';
 
 var smartType = {
+    isFunction: isFunction,
     isObject: isObject,
     isArray: isArray,
     isString: isString,
-    isReged: isRegex,
+    isRegex: isRegex,
     isNumber: isNumber,
     isBoolean: isBoolean,
     isNull: isNull,
     isUndefined: isUndefined,
-    isJson: isJson,
-    getType: getType
+    getType: getType,
+    isOfType: isOfType
+}
+/**
+ * Check if an object is type 'function'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
+function isFunction(element) {
+    return getType(element) === 'function';
 }
 
+/**
+ * Check if an object is type 'object'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isObject(element) {
     return getType(element) === 'object';
 }
 
+/**
+ * Check if an object is type 'array'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isArray(element) {
     return getType(element) === 'array';
 }
 
+/**
+ * Check if an object is type 'string'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isString(element) {
     return getType(element) === 'string';
 }
 
+/**
+ * Check if an object is type 'regexp'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isRegex(element) {
     return getType(element) === 'regexp';
 }
 
+/**
+ * Check if an object is type 'number'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isNumber(element) {
+    if(isNaN(element)) {
+        return false;
+    }
     return getType(element) === 'number';
 }
 
+/**
+ * Check if an object is type 'boolean'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isBoolean(element) {
     return getType(element) === 'boolean';
 }
 
+/**
+ * Check if an object is type 'null'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isNull(element) {
     return getType(element) === 'null';
 }
 
+/**
+ * Check if an object is type 'undefined'
+ *
+ * @param  {Any} element
+ * @return {Boolean}
+ */
 function isUndefined(element) {
     return getType(element) === 'undefined';
 }
 
-function isJson(element) {
-    return getType(element) === 'json';
-}
 
+/**
+ * Get the type of an element
+ *
+ * @param  {Any} element
+ * @return {String}
+ */
 function getType(element) {
     return Object.prototype.toString.call(element).match(/\s([a-zA-Z]+)/)[1].toLowerCase();
+}
+
+/**
+ * Catch all method to check any given type
+ *
+ * @param  {Any} element
+ * @param  {String} type
+ * @return {Boolean}
+ */
+function isOfType(element, type) {
+    return getType(element) === type;
 }
 
 module.exports = smartType;
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,7 +1,21 @@
-'use strict'
+/* jshint -W117, -W030 */
+'use strict';
 
 var expect = require('chai').expect;
 var smartType = require('../index');
+
+
+describe('isFunction', function() {
+    it('should return true if element is a function', function() {
+        var result = smartType.isFunction(function() {});
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not a function', function() {
+        var result = smartType.isFunction({});
+        expect(result).to.be.false;
+    });
+});
 
 describe('isObject', function() {
     it('should return true if element is an object', function() {
@@ -23,6 +37,111 @@ describe('isArray', function() {
 
     it('should return false if element is not an array', function() {
         var result = smartType.isArray();
+        expect(result).to.be.false;
+    });
+});
+
+describe('isString', function() {
+    it('should return true if element is a string', function() {
+        var result = smartType.isString('10');
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not a string', function() {
+        var result = smartType.isString(10);
+        expect(result).to.be.false;
+    });
+});
+
+describe('isRegex', function() {
+    it('should return true if element is a regular expression', function() {
+        var result = smartType.isRegex(/a-z/);
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not a regular expression', function() {
+        var result = smartType.isRegex(function() {});
+        expect(result).to.be.false;
+    });
+});
+
+describe('isNumber', function() {
+    it('should return true if element is a number', function() {
+        var result = smartType.isNumber(10);
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not a number', function() {
+        var result = smartType.isNumber('10');
+        expect(result).to.be.false;
+    });
+
+    it('should return false if element is NaN', function() {
+        var result = smartType.isNumber(NaN);
+        expect(result).to.be.false;
+    })
+});
+
+describe('isBoolean', function() {
+    it('should return true if element is a boolean', function() {
+        var result = smartType.isBoolean(false);
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not a boolean', function() {
+        var result = smartType.isBoolean(10);
+        expect(result).to.be.false;
+    });
+});
+
+describe('isNull', function() {
+    it('should return true if element is a null', function() {
+        var nullVariable = null;
+
+        var result = smartType.isNull(nullVariable);
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not null', function() {
+        var result = smartType.isNull('hello');
+        expect(result).to.be.false;
+    });
+});
+
+describe('isUndefined', function() {
+    it('should return true if element is undefined', function() {
+        var undefinedVariable;
+        var result = smartType.isUndefined(undefinedVariable);
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element is not undefined', function() {
+        var definedVariable = 10;
+        var result = smartType.isUndefined(definedVariable);
+        expect(result).to.be.false;
+    });
+});
+
+describe('getType', function() {
+    it('should return string if element is a string', function() {
+        var result = smartType.getType('10');
+        expect(result).to.equal('string');
+    });
+
+    it('should return number if element is a number', function() {
+        var result = smartType.getType(10);
+        expect(result).to.be.equal('number');
+    });
+});
+
+describe('isOfType', function() {
+    it('should return true if element matches type', function() {
+        var result = smartType.isOfType('', 'string');
+        expect(result).to.be.true;
+    });
+
+    it('should return false if element does not match type', function() {
+        var result = smartType.isOfType(10, 'string');
         expect(result).to.be.false;
     });
 });
